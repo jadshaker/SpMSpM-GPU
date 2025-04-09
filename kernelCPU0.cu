fdff60a830bd07e611890896f739225bc194c286
Artur Baboudjian <artbaboudjian290@gmail.com>
2025-04-09 18:10:17 +0300

    Added comments and change variable names for more clarity

--- kernelCPU0.cu
+++ kernelCPU0.cu
@@ -3,38 +3,59 @@
 
 void spmspm_cpu0(COOMatrix* cooMatrix1, CSRMatrix* csrMatrix1, CSCMatrix* cscMatrix1, COOMatrix* cooMatrix2, CSRMatrix* csrMatrix2, CSCMatrix* cscMatrix2, COOMatrix* cooMatrix3) {
 
-
+    // Initializing some data for the resulting matrix (matrix 3) 
     cooMatrix3->numRows = csrMatrix1->numRows;
     cooMatrix3->numCols = csrMatrix2->numCols;
     cooMatrix3->numNonzeros = 0;
 
+    // Just like in ordinary matrix multiplication, we will go through
+    // all the rows of the first matrix. For each row, we will go through all the
+    // columns of the second matrix. Thus, it is convenient to use the CSR format for
+    // matrix 1 and the CSC format for matrix 2.
     for (unsigned int i = 0; i < csrMatrix1->numRows; i++) {
-        unsigned int row_start = csrMatrix1->rowPtrs[i];
-        unsigned int row_end = csrMatrix1->rowPtrs[i + 1];
+        // Getting the range in which the values reside
+        // in the values array (matrix 1) for each row.
+        unsigned int rowStart = csrMatrix1->rowPtrs[i];
+        unsigned int rowEnd = csrMatrix1->rowPtrs[i + 1];
 
-        for (unsigned int j = 0; j < csrMatrix2->numCols; j++) {
-            unsigned int col_start = cscMatrix2->colPtrs[j];
-            unsigned int col_end = cscMatrix2->colPtrs[j + 1];
+        for (unsigned int j = 0; j < cscMatrix2->numCols; j++) {
+            // Getting the range in which the values reside
+            // in the values array (matrix 2) for each column.
+            unsigned int colStart = cscMatrix2->colPtrs[j];
+            unsigned int colEnd = cscMatrix2->colPtrs[j + 1];
 
             float value = 0.0f;
-            unsigned int row_idx = row_start;
-            unsigned int col_idx = col_start;
 
-            while (row_idx < row_end && col_idx < col_end) {
-                unsigned int col_in_row = csrMatrix1->colIdxs[row_idx];
-                unsigned int row_in_col = cscMatrix2->rowIdxs[col_idx];
+            // Now, we have a row and column, what is left is to 
+            // multiply these non-values together. The only pairs we 
+            // will multiply are the ones that have matching row index and
+            // column index.
 
-                if      (col_in_row < row_in_col)   {row_idx++;} 
-                else if (col_in_row > row_in_col)   {col_idx++;} 
+            // Starting indexes for matrix 1 row and 
+            // matrix 2 column
+            unsigned int rowIdx = rowStart;
+            unsigned int colIdx = colStart;
+            
+            // Loop through the row of matrix 1 and the column of matrix 2
+            // until we reach the end of either of them
+            while (rowIdx < rowEnd && colIdx < colEnd) {
+                unsigned int matrix1_cur_col = csrMatrix1->colIdxs[rowIdx];
+                unsigned int matrix2_cur_row = cscMatrix2->rowIdxs[colIdx];
+
+                if      (matrix1_cur_col < matrix2_cur_row)   {rowIdx++;} 
+                else if (matrix1_cur_col > matrix2_cur_row)   {colIdx++;} 
                 else {
-                    value += csrMatrix1->values[row_idx] * cscMatrix2->values[col_idx];
-                    row_idx++;
-                    col_idx++;
+                    value += csrMatrix1->values[rowIdx] * cscMatrix2->values[colIdx];
+                    rowIdx++;
+                    colIdx++;
                 }
             }
 
-            // In case the result is zero, we wont add it
+            // Don't know if this condition is needed, but in case, we added
+            // this. (CHECK BEFORE SUMBITTING)
             if (value != 0.0f) {
+                // Each value is added to the result matrix in position
+                // (row idx, column idx) of matrix 1 and 2 repectively.
                 cooMatrix3->rowIdxs[cooMatrix3->numNonzeros] = i;
                 cooMatrix3->colIdxs[cooMatrix3->numNonzeros] = j;
                 cooMatrix3->values[cooMatrix3->numNonzeros] = value;
