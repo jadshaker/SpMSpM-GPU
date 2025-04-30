614a20a2bf693cbe13e57c12e9d48da14b0457fd
Jad Shaker <jaadshaker@gmail.com>
2025-04-30 22:48:33 +0300

    Refactor spmspm_cpu0 and spmspm_cpu1 for improved performance; added binary search for efficiency in spmspm_cpu1.

--- kernelCPU0.cu
+++ kernelCPU0.cu
@@ -1,65 +1,29 @@
-
 #include "common.h"
 
-void spmspm_cpu0(COOMatrix* cooMatrix1, CSRMatrix* csrMatrix1, CSCMatrix* cscMatrix1, COOMatrix* cooMatrix2, CSRMatrix* csrMatrix2, CSCMatrix* cscMatrix2, COOMatrix* cooMatrix3) {
+void spmspm_cpu0(COOMatrix *cooMatrix1,
+                 CSRMatrix *csrMatrix1,
+                 CSCMatrix *cscMatrix1,
+                 COOMatrix *cooMatrix2,
+                 CSRMatrix *csrMatrix2,
+                 CSCMatrix *cscMatrix2,
+                 COOMatrix *cooMatrix3)
+{
+    float *matrix = (float *)calloc(cooMatrix3->numRows * cooMatrix3->numCols, sizeof(float));
 
-    // Initializing some data for the resulting matrix (matrix 3) 
-    cooMatrix3->numRows = csrMatrix1->numRows;
-    cooMatrix3->numCols = csrMatrix2->numCols;
-    cooMatrix3->numNonzeros = 0;
+    for (unsigned int i = 0; i < cooMatrix1->numNonzeros; ++i)
+        for (unsigned int j = 0; j < cooMatrix2->numNonzeros; ++j)
+            if (cooMatrix1->colIdxs[i] == cooMatrix2->rowIdxs[j])
+                matrix[cooMatrix1->rowIdxs[i] * cooMatrix3->numCols + cooMatrix2->colIdxs[j]] += cooMatrix1->values[i] * cooMatrix2->values[j];
 
-    // Just like in ordinary matrix multiplication, we will go through
-    // all the rows of the first matrix. For each row, we will go through all the
-    // columns of the second matrix. Thus, it is convenient to use the CSR format for
-    // matrix 1 and the CSC format for matrix 2.
-    for (unsigned int i = 0; i < csrMatrix1->numRows; i++) {
-        // Getting the range in which the values reside
-        // in the values array (matrix 1) for each row.
-        unsigned int rowStart = csrMatrix1->rowPtrs[i];
-        unsigned int rowEnd = csrMatrix1->rowPtrs[i + 1];
-
-        for (unsigned int j = 0; j < cscMatrix2->numCols; j++) {
-            // Getting the range in which the values reside
-            // in the values array (matrix 2) for each column.
-            unsigned int colStart = cscMatrix2->colPtrs[j];
-            unsigned int colEnd = cscMatrix2->colPtrs[j + 1];
-
-            float value = 0.0f;
-
-            // Now, we have a row and column, what is left is to 
-            // multiply these non-values together. The only pairs we 
-            // will multiply are the ones that have matching row index and
-            // column index.
-            
-            // Loop through the rows of matrix 1 and the columns of matrix 2
-            // until we pass through all of them
-            for(unsigned int rowIdx = rowStart; rowIdx<rowEnd; ++rowIdx){
-                unsigned int matrix1_cur_col = csrMatrix1->colIdxs[rowIdx];
-
-                for(unsigned int colIdx= colStart; colIdx<colEnd; ++colIdx){
-                    unsigned int matrix2_cur_row = cscMatrix2->rowIdxs[colIdx];
-
-                    // If the column index of matrix 1 is equal to the row index of matrix 2
-                    // we can multiply the values together.
-                    if (matrix1_cur_col == matrix2_cur_row) {
-                        value += csrMatrix1->values[rowIdx] * cscMatrix2->values[colIdx];
-                    }
-                }
-            }
-
-            // Don't know if this condition is needed, but in case, we added
-            // this. (CHECK BEFORE SUMBITTING)
-            if (value != 0.0f) {
-                // Each value is added to the result matrix in position
-                // (row idx, column idx) of matrix 1 and 2 repectively.
+    for (unsigned int i = 0; i < cooMatrix3->numRows; ++i) {
+        for (unsigned int j = 0; j < cooMatrix3->numCols; ++j) {
+            float val = matrix[i * cooMatrix3->numCols + j];
+            if (val != 0.0f) {
                 cooMatrix3->rowIdxs[cooMatrix3->numNonzeros] = i;
                 cooMatrix3->colIdxs[cooMatrix3->numNonzeros] = j;
-                cooMatrix3->values[cooMatrix3->numNonzeros] = value;
+                cooMatrix3->values[cooMatrix3->numNonzeros] = val;
                 cooMatrix3->numNonzeros++;
-            } 
+            }
         }
     }
 }
-
-
-
--- kernelCPU1.cu
+++ kernelCPU1.cu
@@ -1,11 +1,65 @@
-
+#include <stdio.h>
 #include "common.h"
+#include "matrix.h"
 
-void spmspm_cpu1(COOMatrix* cooMatrix1, CSRMatrix* csrMatrix1, CSCMatrix* cscMatrix1, COOMatrix* cooMatrix2, CSRMatrix* csrMatrix2, CSCMatrix* cscMatrix2, COOMatrix* cooMatrix3) {
-
-
-
-
-
+// Binary search helper function to find first occurrence of a value in a sorted array
+int binarySearchFirst(unsigned int* array, int size, unsigned int target) {
+    int left = 0;
+    int right = size - 1;
+    int result = -1;
+    
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (array[mid] == target) {
+            result = mid;
+            right = mid - 1;  // Continue searching left for first occurrence
+        } else if (array[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    
+    return result;
 }
 
+void spmspm_cpu1(COOMatrix *cooMatrix1,
+                 CSRMatrix *csrMatrix1,
+                 CSCMatrix *cscMatrix1,
+                 COOMatrix *cooMatrix2,
+                 CSRMatrix *csrMatrix2,
+                 CSCMatrix *cscMatrix2,
+                 COOMatrix *cooMatrix3)
+{
+    sortCOOMatrix(cooMatrix2);
+
+    float *matrix = (float *)calloc(cooMatrix3->numRows * cooMatrix3->numCols, sizeof(float));
+
+    for (unsigned int i = 0; i < cooMatrix1->numNonzeros; ++i) {
+        unsigned int col_i = cooMatrix1->colIdxs[i];
+        
+        // Binary search for the first occurrence of col_i in cooMatrix2->rowIdxs
+        int j = binarySearchFirst(cooMatrix2->rowIdxs, cooMatrix2->numNonzeros, col_i);
+        
+        // If found, process all matching elements consecutively
+        if (j != -1) {
+            while (j < cooMatrix2->numNonzeros && cooMatrix2->rowIdxs[j] == col_i) {
+                matrix[cooMatrix1->rowIdxs[i] * cooMatrix3->numCols + cooMatrix2->colIdxs[j]] += 
+                    cooMatrix1->values[i] * cooMatrix2->values[j];
+                j++;
+            }
+        }
+    }
+
+    for (unsigned int i = 0; i < cooMatrix3->numRows; ++i) {
+        for (unsigned int j = 0; j < cooMatrix3->numCols; ++j) {
+            float val = matrix[i * cooMatrix3->numCols + j];
+            if (val != 0.0f) {
+                cooMatrix3->rowIdxs[cooMatrix3->numNonzeros] = i;
+                cooMatrix3->colIdxs[cooMatrix3->numNonzeros] = j;
+                cooMatrix3->values[cooMatrix3->numNonzeros] = val;
+                cooMatrix3->numNonzeros++;
+            }
+        }
+    }
+}
