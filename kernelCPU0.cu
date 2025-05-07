5db914e09c6e7fc8b0c6783bdd473221786e44fc
Jad Shaker <jaadshaker@gmail.com>
2025-05-07 11:52:58 +0300

    Implement CSR * CSR multiplication

--- kernelCPU0.cu
+++ kernelCPU0.cu
@@ -8,20 +8,39 @@ void spmspm_cpu0(COOMatrix *cooMatrix1,
                  CSCMatrix *cscMatrix2,
                  COOMatrix *cooMatrix3)
 {
-    float *matrix = (float *)calloc(cooMatrix3->numRows * cooMatrix3->numCols, sizeof(float));
+    // CSR * CSR implementation
+    float *row = (float *)calloc(cooMatrix3->numCols, sizeof(float));
 
-    for (unsigned int i = 0; i < cooMatrix1->numNonzeros; ++i)
-        for (unsigned int j = 0; j < cooMatrix2->numNonzeros; ++j)
-            if (cooMatrix1->colIdxs[i] == cooMatrix2->rowIdxs[j])
-                matrix[cooMatrix1->rowIdxs[i] * cooMatrix3->numCols + cooMatrix2->colIdxs[j]] += cooMatrix1->values[i] * cooMatrix2->values[j];
+    for (unsigned int rowA = 0; rowA < csrMatrix1->numRows; ++rowA)
+    {
+        memset(row, 0, cooMatrix3->numCols * sizeof(float));
 
-    for (unsigned int i = 0; i < cooMatrix3->numRows; ++i) {
-        for (unsigned int j = 0; j < cooMatrix3->numCols; ++j) {
-            float val = matrix[i * cooMatrix3->numCols + j];
-            if (val != 0.0f) {
-                cooMatrix3->rowIdxs[cooMatrix3->numNonzeros] = i;
-                cooMatrix3->colIdxs[cooMatrix3->numNonzeros] = j;
-                cooMatrix3->values[cooMatrix3->numNonzeros] = val;
+        unsigned int rowStart1 = csrMatrix1->rowPtrs[rowA];
+        unsigned int rowEnd2 = csrMatrix1->rowPtrs[rowA + 1];
+
+        for (unsigned int i = rowStart1; i < rowEnd2; ++i)
+        {
+            float valA = csrMatrix1->values[i];
+            unsigned int rowB = csrMatrix1->colIdxs[i];
+
+            unsigned int rowStart2 = csrMatrix2->rowPtrs[rowB];
+            unsigned int rowEnd2 = csrMatrix2->rowPtrs[rowB + 1];
+
+            for (unsigned int j = rowStart2; j < rowEnd2; ++j)
+            {
+                unsigned int colB = csrMatrix2->colIdxs[j];
+                float valB = csrMatrix2->values[j];
+                row[colB] += valA * valB;
+            }
+        }
+
+        for (unsigned int colIdx = 0; colIdx < cooMatrix3->numCols; ++colIdx)
+        {
+            if (row[colIdx] != 0)
+            {
+                cooMatrix3->rowIdxs[cooMatrix3->numNonzeros] = rowA;
+                cooMatrix3->colIdxs[cooMatrix3->numNonzeros] = colIdx;
+                cooMatrix3->values[cooMatrix3->numNonzeros] = row[colIdx];
                 cooMatrix3->numNonzeros++;
             }
         }
