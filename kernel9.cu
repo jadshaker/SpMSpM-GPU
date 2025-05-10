56768370fd8c54cbf02ad8ade0afe8d98620fa32
Moe-Zbeeb <mhmdzbyb829@gmail.com>
2025-05-11 00:20:30 +0300

    seg fault fixed but time is too bad

--- kernel9.cu
+++ kernel9.cu
@@ -9,55 +9,67 @@ __global__ void spmspm_kernel9(COOMatrix *cooMatrix1,
                                CSRMatrix *csrMatrix2,
                                CSCMatrix *cscMatrix2,
                                COOMatrix *cooMatrix3,
-                               unsigned int numRows1,
-                               unsigned int numRows2,
-                               unsigned int numCols2,
-                               unsigned int numNonzeros1,
-                               unsigned int numNonzeros2)
+                               const unsigned int numRows1,
+                               const unsigned int numRows2,
+                               const unsigned int numCols2,
+                               const unsigned int numNonzeros1,
+                               const unsigned int numNonzeros2)
 {
-    extern __shared__ float rowAcc[];
+    extern __shared__ float row[];
     __shared__ unsigned int nnz;
     if (threadIdx.x == 0) nnz = 0;
-    for (unsigned int i = threadIdx.x; i < cscMatrix1->numRows; i += blockDim.x) rowAcc[i] = 0.0f;
+    for (unsigned int i = threadIdx.x; i < numCols2; i += blockDim.x) row[i] = 0.0f;
     __syncthreads();
-    unsigned int colB = blockIdx.x;
-    if (colB < cscMatrix2->numCols)
+
+    unsigned int rowA = blockIdx.x;
+    for (unsigned int c = threadIdx.x; c < cscMatrix1->numCols; c += blockDim.x)
     {
-        unsigned int startB = cscMatrix2->colPtrs[colB];
-        unsigned int endB   = cscMatrix2->colPtrs[colB + 1];
-        for (unsigned int i = startB + threadIdx.x; i < endB; i += blockDim.x)
+        unsigned int colStart1 = cscMatrix1->colPtrs[c];
+        unsigned int colEnd1   = cscMatrix1->colPtrs[c + 1];
+        for (unsigned int idx = colStart1; idx < colEnd1; ++idx)
         {
-            float valB         = cscMatrix2->values[i];
-            unsigned int colA  = cscMatrix2->rowIdxs[i];
-            unsigned int startA = cscMatrix1->colPtrs[colA];
-            unsigned int endA   = cscMatrix1->colPtrs[colA + 1];
-            for (unsigned int j = startA; j < endA; j++)
+            if (cscMatrix1->rowIdxs[idx] == rowA)
             {
-                float valA = cscMatrix1->values[j];
-                unsigned int rA = cscMatrix1->rowIdxs[j];
-                float prod = valA * valB;
-                if (prod != 0.0f) atomicAdd(&rowAcc[rA], prod);
+                float valA = cscMatrix1->values[idx];
+                for (unsigned int j = 0; j < cscMatrix2->numCols; j++)
+                {
+                    unsigned int colStart2 = cscMatrix2->colPtrs[j];
+                    unsigned int colEnd2   = cscMatrix2->colPtrs[j + 1];
+                    for (unsigned int x2 = colStart2; x2 < colEnd2; x2++)
+                    {
+                        if (cscMatrix2->rowIdxs[x2] == c)
+                        {
+                            float valB = cscMatrix2->values[x2];
+                            float prod = valA * valB;
+                            if (prod != 0.0f)
+                            {
+                                float oldVal = atomicAdd(&row[j], prod);
+                                if (oldVal == 0.0f) atomicAdd(&nnz, 1);
+                            }
+                            break;
+                        }
+                    }
+                }
             }
         }
+    }
+    __syncthreads();
+
+    if (nnz != 0)
+    {
+        __shared__ unsigned int idx;
+        if (threadIdx.x == 0) idx = atomicAdd(&cooMatrix3->numNonzeros, nnz);
         __syncthreads();
-        for (unsigned int i = threadIdx.x; i < cscMatrix1->numRows; i += blockDim.x)
-            if (rowAcc[i] != 0.0f) atomicAdd(&nnz, 1);
-        __syncthreads();
-        if (nnz != 0)
+
+        for (unsigned int i = threadIdx.x; i < numCols2; i += blockDim.x)
         {
-            __shared__ unsigned int baseIdx;
-            if (threadIdx.x == 0) baseIdx = atomicAdd(&cooMatrix3->numNonzeros, nnz);
-            __syncthreads();
-            for (unsigned int i = threadIdx.x; i < cscMatrix1->numRows; i += blockDim.x)
+            float v = row[i];
+            if (v != 0.0f)
             {
-                float v = rowAcc[i];
-                if (v != 0.0f)
-                {
-                    unsigned int pos = atomicAdd(&baseIdx, 1);
-                    cooMatrix3->rowIdxs[pos] = i;
-                    cooMatrix3->colIdxs[pos] = colB;
-                    cooMatrix3->values[pos]  = v;
-                }
+                unsigned int index = atomicAdd(&idx, 1);
+                cooMatrix3->rowIdxs[index] = rowA;
+                cooMatrix3->colIdxs[index] = i;
+                cooMatrix3->values[index]  = v;
             }
         }
     }
@@ -76,12 +88,20 @@ void spmspm_gpu9(COOMatrix *cooMatrix1,
                  unsigned int numNonzeros1,
                  unsigned int numNonzeros2)
 {
-    dim3 block(BLOCK_DIM);
-    dim3 grid(cscMatrix2->numCols);
-    size_t shMemBytes = cscMatrix1->numRows * sizeof(float);
-    spmspm_kernel9<<<grid, block, shMemBytes>>>(cooMatrix1, csrMatrix1, cscMatrix1,
-                                                cooMatrix2, csrMatrix2, cscMatrix2,
+    const dim3 block(BLOCK_DIM);
+    const dim3 grid(numRows1);
+    size_t shMemBytes = numCols2 * sizeof(float);
+
+    spmspm_kernel9<<<grid, block, shMemBytes>>>(cooMatrix1,
+                                                csrMatrix1,
+                                                cscMatrix1,
+                                                cooMatrix2,
+                                                csrMatrix2,
+                                                cscMatrix2,
                                                 cooMatrix3,
-                                                numRows1, numRows2, numCols2,
-                                                numNonzeros1, numNonzeros2);
+                                                numRows1,
+                                                numRows2,
+                                                numCols2,
+                                                numNonzeros1,
+                                                numNonzeros2);
 }
